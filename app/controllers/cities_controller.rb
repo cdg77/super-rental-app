726906aaf35fa37ed2de34afde62d1d1237417f7
cdg77 <cdegregorio@gmail.com>
2015-10-27 18:42:40 -0700

    add new cities with a name, state, and country properties

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -9,5 +9,22 @@ class CitiesController < ApplicationController
     render :show
   end
 
+  def new
+    @city = City.new
+    render :new
+  end
 
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      redirect_to  cities_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def city_params
+    params.require(:city).permit(:name, :state, :country)
+  end
 end
